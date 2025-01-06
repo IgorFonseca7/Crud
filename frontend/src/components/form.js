be8b01bac695932524dc4286f8414712f361cf10
IgorFonseca7 <igor.galaxy2015@gmail.com>
2025-01-06 16:49:54 -0300

    fix: correct styling issues in form component and update global styles

--- frontend/src/components/form.js
+++ frontend/src/components/form.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const FormContainer = styled.form`
     display: flex;
     align-items: flex-end;
-    gpa: 10px;
+    gap: 10px; /* Correção da propriedade */
     flex-wrap: wrap;
     background: #fff;
     padding: 20px;
@@ -27,7 +27,7 @@ const Input = styled.input`
 
 const Label = styled.label``;
 
-const button = styled.button`
+const Button = styled.button`
     padding: 10px;
     cursor: pointer;
     border-radius: 5px;
@@ -41,25 +41,25 @@ const Form = ({ onEdit }) => {
     const ref = useRef();
 
     return (
-        <FormContainer >
+        <FormContainer>
             <InputArea>
-                <label>Nome</label>
-                <input name= "nome" />
+                <Label>Nome</Label>
+                <Input name="nome" />
             </InputArea>
             <InputArea>
-                <label>E-mail</label>
-                <input name= "email" type="email" />
-                </InputArea>
+                <Label>E-mail</Label>
+                <Input name="email" type="email" />
+            </InputArea>
             <InputArea>
-                <label>Telefone</label>
-                <input name= "fone" />
-                </InputArea>
+                <Label>Telefone</Label>
+                <Input name="fone" />
+            </InputArea>
             <InputArea>
-                <label>Data de Nascimento</label>
-                <input name= "data_nascimento" type="date" />
-                </InputArea>
+                <Label>Data de Nascimento</Label>
+                <Input name="data_nascimento" type="date" />
+            </InputArea>
 
-            <button type="submit">SALVAR</button>
+            <Button type="submit">SALVAR</Button>
         </FormContainer>
     );
 };
--- frontend/src/styles/GlobalStyle.js
+++ frontend/src/styles/GlobalStyle.js
@@ -5,7 +5,7 @@ const GlobalStyle = createGlobalStyle`
     * {
         margin: 0;
         padding: 0;
-        font-family: 'poppins', sans-serif;
+        font-family: 'Poppins', sans-serif;
     }
 
     body {
